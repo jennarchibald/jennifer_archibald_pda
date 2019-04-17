735eb24cbbd19e200a8503e3d1fd7e871eed64f9
jennarchibald <codeclan@CodeClans-MacBook-Pro.local>
2019-04-17 08:55:30 +0100

    further screenshots added

--- PDA_Static_and_Dynamic_Task_A/testing_task_2_2.rb
+++ PDA_Static_and_Dynamic_Task_A/testing_task_2_2.rb
@@ -16,11 +16,11 @@ class CardGame
   end
 
   def highest_card(card1, card2)
-    if card1.value > card2.value
-      return card1
-    else
-      card2
-    end
+  if card1.value > card2.value
+    return card1
+  else
+    return card2
+  end
   end
 
   def cards_total(cards)
@@ -28,7 +28,6 @@ class CardGame
     for card in cards
       total += card.value
     end
-    return "You have a total of #{total}"
+    return "You have a total of " + total.to_s
   end
-
 end
